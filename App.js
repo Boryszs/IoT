a5a2537bcfbcd9effad7de3467f00ee2695a3498
Boryszs <12damie@gmail.com>
2021-01-24 04:48:29 +0100

    Add Splash Screen. Change App file to class,

--- App.js
+++ App.js
@@ -21,48 +21,59 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import DeviceScreen from './screen/DeviceScreen';
 import ConnectionScreen from './screen/ConnectionScreen';
 import NewDeviceScreen from './screen/NewDeviceScreen';
-
+import SplashScreen from 'react-native-splash-screen';
 const SettingsStack = createStackNavigator();
-
-function SettingsStackScreen(){
-  return (
-
-    <Tabs.Navigator tabBarOptions={{
-      unmountOnBlur:true,
-      activeBackgroundColor: "grey",
-      activeTintColor: "black",
-      tabStyle: {
-        backgroundColor: '#C8C8C8',
-        borderWidth: 1,
-        borderColor: 'black',
-      }, labelStyle: {
-        fontSize: 16,
-        color: 'black',
-        textAlignVertical: 'center',
-        flex: 1,
-      }
-    }}>
-
-      <Tabs.Screen name="Device" component={DeviceScreen} unmountOnBlur={true}  options={{ unmountOnBlur: true }} />
-      <Tabs.Screen name="Connection" component={ConnectionScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }} />
-    </Tabs.Navigator>
-  );
-}
-
-
 const Tabs = createBottomTabNavigator();
-const App = () => {
-  return (
-    SettingsStackScreen(),
-     <NavigationContainer>
-          <SettingsStack.Navigator unmountOnBlur={true} initialRouteName={"Device"} screenOptions={{
-        headerShown: false
-      }}>
-         <SettingsStack.Screen name="Device" component={SettingsStackScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }}/>
-         <SettingsStack.Screen name="NewDeviceScreen" component={NewDeviceScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }}/>
-      </SettingsStack.Navigator>
-    </NavigationContainer>
-  );
-};
+export default class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+
+  SettingsStackScreen() {
+    return (
+
+      <Tabs.Navigator tabBarOptions={{
+        unmountOnBlur: true,
+        activeBackgroundColor: "grey",
+        activeTintColor: "black",
+        tabStyle: {
+          backgroundColor: '#C8C8C8',
+          borderWidth: 1,
+          borderColor: 'black',
+        }, labelStyle: {
+          fontSize: 16,
+          color: 'black',
+          textAlignVertical: 'center',
+          flex: 1,
+        }
+      }}>
+
+        <Tabs.Screen name="Device" component={DeviceScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }} />
+        <Tabs.Screen name="Connection" component={ConnectionScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }} />
+      </Tabs.Navigator>
+    );
+  }
+
+
+  render() {
+    return (
+    //  SettingsStackScreen(),
+      <NavigationContainer>
+        <SettingsStack.Navigator unmountOnBlur={true} initialRouteName={"Device"} screenOptions={{
+          headerShown: false
+        }}>
+          <SettingsStack.Screen name="Device" component={this.SettingsStackScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }} />
+          <SettingsStack.Screen name="NewDeviceScreen" component={NewDeviceScreen} unmountOnBlur={true} options={{ unmountOnBlur: true }} />
+        </SettingsStack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
